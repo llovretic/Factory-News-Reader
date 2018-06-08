b3af86aa4cb9d5630045404fbfc4195716dc0a06
Luka Lovretic <llovretic@lukas-imac>
2018-06-08 09:29:53 +0200

    Initial Commit

---
+++ Factory News Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Factory News Reader
+//
+//  Created by Luka Lovretic on 08/06/2018.
+//  Copyright © 2018 Luka Lovretic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
